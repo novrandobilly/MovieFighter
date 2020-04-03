2ea1b410f5d3aaadee4d2b14892218f998ab3636
Billy Novrando <novrandobilly@gmail.com>
2020-04-03 10:07:25 +0700

    Setup axios and fetch sample api from omdbapi

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('https://www.omdbapi.com/', {
+		params: {
+			apikey: 'e55cd6f4',
+			// s: 'avengers'
+			i: 'tt4154756'
+		}
+	});
+	console.log(response.data);
+};
+
+fetchData();
