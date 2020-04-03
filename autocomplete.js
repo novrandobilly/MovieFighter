a394064b138f3e007db2c99e50c0af04618a2cf0
Billy Novrando <novrandobilly@gmail.com>
2020-04-03 21:55:16 +0700

    Finish creating reusable autocomplete.js pt 2 (finished)

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) => {
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
 	root.innerHTML = `
     <label><b>Search For Movie</b></label>
     <input type="text" class="input">
@@ -15,24 +15,24 @@ const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) 
 
 	//==========================================================================
 	const onInput = debounce(async (event) => {
-		const movies = await fetchData(event.target.value);
+		const items = await fetchData(event.target.value);
 
-		if (!movies.length) {
+		if (!items.length) {
 			dropdown.classList.remove('is-active');
 			return;
 		}
 
 		resultsWrapper.innerHTML = '';
 		dropdown.classList.add('is-active');
-		for (let movie of movies) {
+		for (let item of items) {
 			const option = document.createElement('a');
 
 			option.classList.add('dropdown-item');
-			option.innerHTML = renderOption(movie);
+			option.innerHTML = renderOption(item);
 			option.addEventListener('click', () => {
 				dropdown.classList.remove('is-active');
-				input.value = inputValue(movie);
-				onOptionSelect(movie);
+				input.value = inputValue(item);
+				onOptionSelect(item);
 			});
 			resultsWrapper.appendChild(option);
 		}
