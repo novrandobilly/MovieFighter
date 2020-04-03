d95247cd19ce8604e7a918805c53c54d23c7c493
Billy Novrando <novrandobilly@gmail.com>
2020-04-03 17:47:38 +0700

    Create first autocomplete (not reusable not refactored yet)

--- index.js
+++ index.js
@@ -5,13 +5,117 @@ const fetchData = async (searchTerm) => {
 			s: `${searchTerm}`
 		}
 	});
-	console.log(response.data);
+
+	if (response.data.Error) {
+		return [];
+	}
+	return response.data.Search;
 };
 
-const input1 = document.querySelector('#input1');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For Movie</b></label>
+    <input type="text" class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
-const onInput = debounce((event) => {
-	fetchData(event.target.value);
+const input = document.querySelector('.input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+//==========================================================================
+const onInput = debounce(async (event) => {
+	const movies = await fetchData(event.target.value);
+
+	if (!movies.length) {
+		dropdown.classList.remove('is-active');
+		return;
+	}
+
+	resultsWrapper.innerHTML = '';
+	dropdown.classList.add('is-active');
+	for (let movie of movies) {
+		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+        <img src='${imgSrc}' />
+        ${movie.Title}
+        `;
+		option.addEventListener('click', () => {
+			dropdown.classList.remove('is-active');
+			input.value = movie.Title;
+			onMovieSelect(movie);
+		});
+		resultsWrapper.appendChild(option);
+	}
+}, 500);
+//==========================================================================
+
+input.addEventListener('input', onInput);
+
+document.addEventListener('click', (event) => {
+	if (!root.contains(event.target)) {
+		dropdown.classList.remove('is-active');
+	}
 });
 
-input1.addEventListener('input', onInput);
+const onMovieSelect = async (movie) => {
+	const followUp = await axios.get('https://www.omdbapi.com/', {
+		params: {
+			apikey: 'e55cd6f4',
+			i: `${movie.imdbID}`
+		}
+	});
+
+	document.querySelector('#summary').innerHTML = movieTemplate(followUp.data);
+};
+
+const movieTemplate = (movieDetail) => {
+	return `
+    <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}" alt="">
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+    `;
+};
