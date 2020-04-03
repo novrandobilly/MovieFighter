04967f79d392b02d74586c2f1cd8cb69753e346a
Billy Novrando <novrandobilly@gmail.com>
2020-04-03 11:01:15 +0700

    Add autosearch fetch for input 1

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('https://www.omdbapi.com/', {
 		params: {
 			apikey: 'e55cd6f4',
-			// s: 'avengers'
-			i: 'tt4154756'
+			s: `${searchTerm}`
 		}
 	});
 	console.log(response.data);
 };
 
-fetchData();
+const input1 = document.querySelector('#input1');
+let timeOutId;
+
+input1.addEventListener('input', (event) => {
+	if (timeOutId) {
+		clearTimeout(timeOutId);
+	}
+	timeOutId = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 750);
+});
